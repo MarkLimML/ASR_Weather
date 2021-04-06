8ee6d1bd736d86db1a0fd6345943d85621c5aa52
MarkLimML <43471388+MarkLimML@users.noreply.github.com>
2021-04-06 17:04:30 +0800

    new output added

--- local/check_new_words.py
+++ local/check_new_words.py
@@ -24,15 +24,17 @@ lex.write(b"<UNK> SPN\n")
 i = 0
 
 with open("words.txt") as f:
-	for line in f:
-		line = line.strip()
-		if line in ref.keys():
-			for pron in ref[line]:
-				lex.write(bytes(line, 'utf-8') + b" " + bytes(pron, 'utf-8')+b"\n")
-		else:
-			i += 1
-			lex.write(bytes(line, 'utf-8') + b"\n")
-			print("Word not in lexicon:" + line + "\n")
+	with open("nonlex.txt","r") as n:
+		for line in f:
+			line = line.strip()
+			if line in ref.keys():
+				for pron in ref[line]:
+					lex.write(bytes(line, 'utf-8') + b" " + bytes(pron, 'utf-8')+b"\n")
+			else:
+				i += 1
+				lex.write(bytes(line, 'utf-8') + b"\n")
+				print("Word not in lexicon:" + line + "\n")
+				n.write(line + "\n")
 			
 			
 print(i, "words not included in lexicon")
