f940ed56e37482f8e6ef12d7978f56f2090394e6
MarkLimML <marklouislim@gmail.com>
2021-05-03 14:39:37 +0800

    Update test.sh

--- local/test.sh
+++ local/test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-for i in $(seq 1 100); do
-    sleep 2 
-    echo $i
-done | whiptail --title 'Test script' --gauge 'Running...' 6 60 0
+curdate="$(date '+%m_%d_%Y')"
+tmp=$curdate
+for i in 2 3 4 5 6 7 8; do
+	if [ -f "exp_log/log_$curdate.txt" ]; then
+		echo "log_$curdate exists"
+		curdate="$tmp"_"$i"
+		echo $curdate
+	else
+		echo "creating log_$curdate"
+		touch exp_log/log_$curdate.txt
+		break
+	fi
+done
